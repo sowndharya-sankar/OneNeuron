cf460d7443a8a56c2b6b8ec18ca04ecea8454cda
sowndharya-sankar <sowndharyasankar98@gmail.com>
2022-02-06 19:03:49 +0530

    and model file commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from utils.model import Perceptron
-from utils.all_utils import prepare_data
+from utils.all_utils import prepare_data, save_model, save_plot
 import numpy as np
 import joblib
 
@@ -21,3 +21,6 @@ model = Perceptron(eta=ETA, epochs=EPOCHS)
 model.fit(X, y)
 
 _ = model.total_loss()
+
+save_model(model, "and.model")
+save_plot(df, "and.png", model)
