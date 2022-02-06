2c95642ce8aa071b2664f71ebc50ab7a7ac57b80
sowndharya-sankar <sowndharyasankar98@gmail.com>
2022-02-06 19:34:26 +0530

    code organised

--- main.py
+++ main.py
@@ -4,23 +4,27 @@ from utils.all_utils import prepare_data, save_model, save_plot
 import numpy as np
 import joblib
 
-AND = {
-    "x1": [0,0,1,1],
-    "x2": [0,1,0,1],
-    "y": [0,0,0,1],
-}
 
-df = pd.DataFrame(AND)
+def main(data, eta, epochs, modelfilename, plotfilename):
+    df = pd.DataFrame(data)
 
-X,y = prepare_data(df)
+    X, y = prepare_data(df)
 
-ETA = 0.3 # 0 and 1
-EPOCHS = 10
+    model = Perceptron(eta=eta, epochs=epochs)
+    model.fit(X, y)
 
-model = Perceptron(eta=ETA, epochs=EPOCHS)
-model.fit(X, y)
+    _ = model.total_loss()
 
-_ = model.total_loss()
+    save_model(model, modelfilename)
+    save_plot(df, plotfilename, model)
 
-save_model(model, "and.model")
-save_plot(df, "and.png", model)
+
+if __name__ == '__main__':
+    AND = {
+        "x1": [0, 0, 1, 1],
+        "x2": [0, 1, 0, 1],
+        "y": [0, 0, 0, 1],
+    }
+    ETA = 0.3  # 0 and 1
+    EPOCHS = 10
+    main(data=AND, eta=ETA, epochs=EPOCHS, modelfilename="and.model", plotfilename="and.png")
